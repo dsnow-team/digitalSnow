ab43ba2ada693bd7175a3762740fd2361eef962c
Tristan Roussillon <tristan.roussillon@liris.cnrs.fr>
2012-01-09 11:49:42 +0100

    adding method with weickert evolver in image-blurring for comparison

--- deformations/imageBlurring.cpp
+++ deformations/imageBlurring.cpp
@@ -19,16 +19,17 @@ namespace po = boost::program_options;
 using namespace Z2i;
 
 #include "ExactDiffusionEvolver.h"
+#include "WeickertKuhneEvolver.h"
 
 template< typename TImage >
-void write ( const TImage& img, std::string basename ) 
+void write ( const TImage& img, std::string basename, int min = 0, int max = 255 ) 
 {
 
     typedef GradientColorMap<typename TImage::Value, DGtal::CMAP_GRAYSCALE> ColorMap; 
 
     std::stringstream s; 
     s << basename << ".pgm"; 
-    PNMWriter<TImage,ColorMap>::exportPGM( s.str(), img, 0, 255, true );
+    PNMWriter<TImage,ColorMap>::exportPGM( s.str(), img, min, max, true );
 } 
 
 
@@ -45,7 +46,9 @@ int main(int argc, char** argv)
   general_opt.add_options()
     ("help,h", "display this message")
     ("inputImage,i",  po::value<string>(), "gray level image (.pgm)" )
-    ("sigma,s",  po::value<double>()->default_value(0.01), "controls the amount of blurring (between 0.001 and 0.1)" )
+    ("algo,a",  po::value<string>()->default_value("exact"), 
+"can be: \n 'exact' (sigma between 0.001 and 0.1) \n or 'weickert' (sigma between 0.3 and 3)" )
+    ("sigma,s",  po::value<double>()->default_value(0.01), "controls the amount of blurring " )
     ("outputFile,o",   po::value<string>()->default_value("output"), "Output file basename" );
 
   po::variables_map vm;
@@ -55,7 +58,7 @@ int main(int argc, char** argv)
     {
       trace.info()<< "Image-blurring" << std::endl
       << "Basic usage: "<<std::endl
-      << argv[0] << " -s 0.02" << std::endl
+      << argv[0] << " -a exact -s 0.02 " << std::endl
       << general_opt << "\n";
       return 0;
     }
@@ -69,6 +72,11 @@ int main(int argc, char** argv)
       return 0; 
     }
   inputFilename = vm["inputImage"].as<std::string>();
+
+  //algo
+  std::string algo; 
+  if (!(vm.count("algo"))) trace.info() << "default algorithm: exact" << std::endl; 
+  algo = vm["algo"].as<string>(); 
 
   //sigma
   double sigma; 
@@ -88,12 +96,40 @@ int main(int argc, char** argv)
   //Diffusion
   trace.info() << "sigma: " << sigma << std::endl; 
   double tstep = (sigma*sigma)/2.0; 
-  trace.info() << "time step: " << tstep << std::endl; 
-  ExactDiffusionEvolver<GrayImage> e; 
-  e.update(img,tstep);
+  trace.info() << "time step: " << tstep << std::endl;
 
-  //writting image
-  write(img, outputFilename); 
+  if (algo.compare("exact")==0)
+  {
+    ExactDiffusionEvolver<GrayImage> e; 
+    e.update(img,tstep);
+    write(img, outputFilename); 
+
+  } else if (algo.compare("weickert")==0)
+  {
+
+    //data functions
+    ImageContainerBySTLVector<Domain,double> a(img.lowerBound(),img.upperBound()); 
+    std::fill(a.begin(),a.end(), 1.0 );  
+    ImageContainerBySTLVector<Domain,double> b(img.lowerBound(),img.upperBound()); 
+    std::fill(b.begin(),b.end(), 1.0 );  
+    ImageContainerBySTLVector<Domain,double> g(img.lowerBound(),img.upperBound()); 
+    std::fill(g.begin(),g.end(), 1.0 );  
+
+    // pb of types unsigned char / double
+    ImageContainerBySTLVector<Domain,double> img2(img.lowerBound(),img.upperBound());
+    std::copy(img.begin(), img.end(), img2.begin());
+ 
+    //evolver
+    WeickertKuhneEvolver<ImageContainerBySTLVector<Domain,double> > e(a,b,g,0,1);
+    e.update(img2,tstep);
+
+    // pb of types unsigned char / double
+    std::copy(img2.begin(), img2.end(), img.begin());
+    write(img, outputFilename, 0, 128); 
+
+  } else trace.error() << "unknown algo. Try 'exact' or 'weickert' " << std::endl;
+
+
 
 	return 1;
 }
