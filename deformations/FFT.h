5c7d767094e8ebfb4669ff2531ce8b655d65c498
troussil <tristan.roussillon@liris.cnrs.fr>
2013-04-19 14:52:21 +0200

    evolution with respect to DGtal: distance transform and PointVector operator access.at() into []

--- deformations/FFT.h
+++ deformations/FFT.h
@@ -6,6 +6,7 @@
 #include <complex>
 
 #include <DGtal/images/CImage.h>
+#include "DGtal/images/ImageContainerBySTLVector.h"
 
 namespace DGtal{
 
--- deformations/deformationFunctions.h
+++ deformations/deformationFunctions.h
@@ -82,6 +82,8 @@ void initWithFlower(TImage& img, const typename TImage::Point& c, double r, doub
 
 #include "DGtal/geometry/volumes/distance/DistanceTransformation.h"
 
+#include <functional>
+
 template< typename TValue >
 TValue aFunction(const TValue& v)
 {
@@ -89,49 +91,114 @@ TValue aFunction(const TValue& v)
   else return 0;
 }
 
+struct Predicate 
+{
+  template< typename TValue >
+  bool operator()(const TValue& v)
+  {
+    if (v == 0) return true; 
+    else return false;
+  }
+}; 
+
 template< typename TImage >
 void initWithDT(const TImage& inputImage, ImageContainerBySTLVector<typename TImage::Domain,double>& outputImage)
 {
+  typedef typename TImage::Domain::Space Space; 
 
-  //inv
+  //domain
   typename TImage::Domain d = inputImage.domain(); 
-  TImage rInputImage(d); 
-  std::transform(inputImage.begin(), inputImage.end(),
-                 rInputImage.begin(), aFunction<typename TImage::Value> ); 
-  //DT 
-  typedef  DistanceTransformation<TImage, 2> DT;
-  DT dt;
-  typename DT::OutputImage dtImage = dt.compute ( inputImage );
-  typename DT::OutputImage rDtImage = dt.compute ( rInputImage );
 
-  //deduce the signed distance function 
-  typename TImage::Domain::ConstIterator cIt = d.begin(); 
-  typename TImage::Domain::ConstIterator cItEnd = d.end(); 
-  for ( ; cIt != cItEnd; ++cIt)
-  { //for each domain point
+  //metric
+  typedef ExactPredicateLpSeparableMetric<Space, 2> L2Metric;
+  L2Metric l2; 
 
-    typedef typename TImage::Point Point; 
-    Point p( *cIt ); //point p
+  //Foreground 
+  //helpers
+  typedef Thresholder<typename TImage::Value, true, true> Binarizer; 
+  Binarizer binarizer(0);
+  typedef PointFunctorPredicate<TImage,Binarizer> PredicateOnPoints; 
+  PredicateOnPoints predicate(inputImage, binarizer); 
+  //DT
+  typedef DistanceTransformation<Space, PredicateOnPoints, L2Metric > DT;
+  DT dt( d, predicate, l2);
 
-    double dist = 0; //signed distance
-    if ( dtImage( p ) == 0 )
-      {
-        if ( rDtImage( p ) == 0 )
-          {
-            std::cerr << "Error init with DT" << std::endl;  
-          }
-        else
-          {
-            dist = std::sqrt( (double) rDtImage( p ) ) - 0.5;  
-          }
-      }
-    else
-      {
-        dist = - ( std::sqrt( (double) dtImage( p ) ) - 0.5 );  
-      }
+  //Background
+  //helpers
+  typedef Thresholder<typename TImage::Value, false, false> Binarizer2; 
+  Binarizer2 binarizer2(0);
+  typedef PointFunctorPredicate<TImage,Binarizer2> PredicateOnPoints2; 
+  PredicateOnPoints2 predicate2(inputImage, binarizer2); 
+  //DT
+  typedef DistanceTransformation<Space, PredicateOnPoints2, L2Metric > DT2;
+  DT2 dt2( d, predicate2, l2);
 
-    outputImage.setValue(p, (double) dist);  
-  }
+  //Signed distance 
+  typename ImageContainerBySTLVector<typename TImage::Domain,double>::OutputIterator out; 
+  for ( typename DT::ConstRange::ConstIterator 
+	  it = dt.constRange().begin(), itend = dt.constRange().end();
+	it != itend; ++it)
+    {
+      double dist = (*it); 
+      if (dist != 0) 
+	dist = std::sqrt( dist ) - 0.5;  
+      *out++ = dist; 
+      // outputImage.setValue(p, dist);  
+    }
+
+  for ( typename DT2::ConstRange::ConstIterator 
+	  it = dt2.constRange().begin(), itend = dt2.constRange().end();
+	it != itend; ++it)
+    {
+      double dist = (*it); 
+      if (dist != 0) 
+	{ //only write if dist != 0
+	  dist = std::sqrt( dist ) - 0.5;  
+	  *out++ = dist; 
+	  // outputImage.setValue(p, dist);  
+	}
+      else 
+	out++; 
+    }
+
+  // typedef  DistanceTransformation<TImage, 2> DT;
+  // DT dt;
+  // typename DT::OutputImage dtImage = dt.compute ( inputImage );
+
+  // //inv
+  // TImage rInputImage(d); 
+  // std::transform(inputImage.begin(), inputImage.end(),
+  //                rInputImage.begin(), aFunction<typename TImage::Value> ); 
+  // typename DT::OutputImage rDtImage = dt.compute ( rInputImage );
+
+  // //deduce the signed distance function 
+  // typename TImage::Domain::ConstIterator cIt = d.begin(); 
+  // typename TImage::Domain::ConstIterator cItEnd = d.end(); 
+  // for ( ; cIt != cItEnd; ++cIt)
+  // { //for each domain point
+
+  //   typedef typename TImage::Point Point; 
+  //   Point p( *cIt ); //point p
+
+  //   double dist = 0; //signed distance
+  //   if ( dtImage( p ) == 0 )
+  //     {
+  //       if ( rDtImage( p ) == 0 )
+  //         {
+  //           std::cerr << "Error init with DT" << std::endl;  
+  //         }
+  //       else
+  //         {
+  //           dist = std::sqrt( (double) rDtImage( p ) ) - 0.5;  
+  //         }
+  //     }
+  //   else
+  //     {
+  //       dist = - ( std::sqrt( (double) dtImage( p ) ) - 0.5 );  
+  //     }
+
+  //   outputImage.setValue(p, (double) dist);  
+  // }
 }
 
 class Profile {
